e3588c7e487a9c6c95efeef46986600f43b7d344
Takada Yushi <takada.yushi@plus-zero.co.jp>
2024-05-04 15:23:17 +0900

    add example

---
+++ prisma/docs/models.md
@@ -0,0 +1,24 @@
+Models:
+
+- User
+    - id
+    - email
+    - type
+    - name
+    - posts
+- Post
+    - id
+    - createdAt
+    - updatedAt
+    - title
+    - content
+    - published
+    - viewCount
+    - author
+    - authorId
+
+Enums:
+
+- UserType
+    - ADMIN
+    - USER
---
+++ prisma/graphql/helpers.js
@@ -0,0 +1,27 @@
+/**
+ * @this {import('@prisma/generator-helper').DMMF.Field}
+ * @returns {string}
+ */
+export function type() {
+	return `${this.isId ? 'ID' : this.type}${this.isRequired ? '!' : ''}`
+}
+
+/**
+ * @this {import('@prisma/generator-helper').DMMF.SchemaArg}
+ * @returns {string}
+ */
+export function inputType() {
+	/**
+	 *
+	 * @param {import('@prisma/generator-helper').DMMF.InputTypeRef} field
+	 * @returns number
+	 */
+	const priority = (field) =>
+		(field.isList ? 4 : 0) +
+		['inputObjectTypes', 'enumTypes', 'scalar', 'fieldRefTypes'].indexOf(
+			field.location,
+		)
+	return type.bind(
+		Array.from(this.inputTypes).sort((a, b) => priority(a) - priority(b))[0],
+	)()
+}
